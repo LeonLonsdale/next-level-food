d2feb29095adefec87eea5c472a1658dcff13d70
LeonLonsdale <leon.coding@icloud.com>
2024-04-25 16:15:26 +0100

    Community - Community page

--- components/images/image-slideshow.tsx
+++ components/images/image-slideshow.tsx
@@ -23,9 +23,21 @@ const images = [
   { image: tomatoSaladImg, alt: "A delicious tomato salad" },
 ];
 
+/*
+- Images are stacked on top of each other so that only 1 is visible.
+- An interval is set for 5 seconds in an Effect to increment state that tracks 
+  the current image to display.
+- When the interval updates the currentImageIndex state, the component refreshes.
+- images[] is mapped over and the index of the current iteration compared with
+  the currentImageIndex state. Where it matchces, the active class is applied.
+- The active class raises the z-index to bring the image to the front.
+*/
+
 export default function ImageSlideshow() {
   const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
 
+  // Update the currentImageIndex state every 5 seconds
+  // increment by 1 each interval or reset to 0 if the current index is the last
   useEffect(() => {
     const interval = setInterval(
       () =>
@@ -39,6 +51,9 @@ export default function ImageSlideshow() {
 
   return (
     <div className={classes.slideshow}>
+      {/* map over the image array and apply the active class where the index
+      is equal to the currentImageIndex state. Active class increaes the 
+      z-index bringing the image to the top */}
       {images.map((image, index) => (
         <Image
           key={index}
