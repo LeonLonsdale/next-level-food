a1a262bae3beb305143ae189cdf7ea9b64108f9b
LeonLonsdale <leon.coding@icloud.com>
2024-05-01 19:02:35 +0100

    App - handle 404 page not found

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not Found!</h1>
+      <p>We couldn't find that page, maybe it doesn't exist - who knows!</p>
+    </main>
+  );
+}
