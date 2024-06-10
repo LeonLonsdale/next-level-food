69c01b1ac92182119b058db9c79ddd0dc44da2db
LeonLonsdale <leon.coding@icloud.com>
2024-06-10 10:53:16 +0100

    minor refactor

--- lib/types.ts
+++ lib/types.ts
@@ -1,13 +1,16 @@
 import { z } from "zod";
 import { imageUploadSchema, mealSchema, formMealSchema } from "./validators";
 
-export type ImageUpload = z.infer<typeof imageUploadSchema>;
+// meal types
 export type Meal = z.infer<typeof mealSchema>;
+
+// share meal form types
+export type ImageUpload = z.infer<typeof imageUploadSchema>;
 export type FormMeal = z.infer<typeof formMealSchema>;
 
 export interface NewMealFormState {
   message: string;
-  errors: Partial<{
-    [key: string]: string[];
-  }>;
+  errors: {
+    [key: string]: string[] | undefined;
+  };
 }
